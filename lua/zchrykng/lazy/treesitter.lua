567be21c03b8e62be71d550d60ae779c812b3da1
Zachary King <zchrykng@fastmail.com>
2025-02-26 16:00:05 -0500

    capture current

--- lua/zchrykng/lazy/treesitter.lua
+++ lua/zchrykng/lazy/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
+    config = function ()
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
@@ -11,7 +11,7 @@ return {
 		"bash", "python", "go" },
           sync_install = false,
           highlight = { enable = true },
-          indent = { enable = true },  
+          indent = { enable = true },
         })
     end
 }
